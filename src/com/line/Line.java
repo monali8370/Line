cc072db5e0f41f5fe879933da4c875bd31f67d4e
monali8370 <monalishinde952@gmail.com>
2023-12-08 10:47:47 +0530

    Calculated:length of line

--- src/com/line/Line.java
+++ src/com/line/Line.java
@@ -1,7 +1,16 @@
 package com.line;
-
+import static java.lang.Math.sqrt;
 public class Line {
+    static int x1 = 2;
+    static int x2 = 5;
+    static int y1 = 3;
+    static int y2 = 5;
     public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparision Program");
+
+        System.out.println("Welcome Line Comparison Program");
+
+
+        double LengthOfLine = sqrt( (x2 - x1) ^ 2 + (y2- y1) ^ 2);
+        System.out.println("Length of line is "+LengthOfLine );
     }
 }
