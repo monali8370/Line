90db397346b1bf38a48c0eb3868cdb2c5e882c05
monali8370 <monalishinde952@gmail.com>
2023-12-08 10:56:30 +0530

    checked:two line equality

--- src/com/line/Line.java
+++ src/com/line/Line.java
@@ -1,16 +1,30 @@
 package com.line;
 import static java.lang.Math.sqrt;
+import java.util.*;
 public class Line {
-    static int x1 = 2;
-    static int x2 = 5;
-    static int y1 = 3;
-    static int y2 = 5;
     public static void main(String[] args) {
-
-        System.out.println("Welcome to Line Comparison Program");
-
-
-        double LengthOfLine = sqrt( (x2 - x1) ^ 2 + (y2- y1) ^ 2);
-        System.out.println("Length of line is "+LengthOfLine );
+        System.out.println("Welcome to line comparison computation program");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter values for line 2 ");
+        int x21 = sc.nextInt();
+        int x22 = sc.nextInt();
+        int y21 = sc.nextInt();
+        int y22 = sc.nextInt();
+        System.out.println("Enter values for line 1 ");
+        int x11 = sc.nextInt();
+        int x12 = sc.nextInt();
+        int y11 = sc.nextInt();
+        int y12 = sc.nextInt();
+        double length1 = sqrt(Math.pow((x12-x11),2)+Math.pow((y12-y11),2));
+        double length2 = sqrt(Math.pow((x22-x21),2)+Math.pow((y22-y21),2));
+        System.out.println("Length of line 1 is "+length1);
+        System.out.println("Length of line 2 is "+length2);
+        Double dLen1 = new Double(length1);
+        Double dLen2 = new Double(length2);
+        if ( dLen1.equals( dLen2)){
+            System.out.println("Two lines are equal ");
+        }else {
+            System.out.println( "Two lines are not equal");
+        }
     }
 }
